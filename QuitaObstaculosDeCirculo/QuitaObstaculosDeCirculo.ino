f9182ccf1762c3f80dcc8c6cc3d7ae886ec71fda
nLaugas <nicolasdaniellaugas@gmail.com>
2018-09-04 00:08:25 -0300

    debugged to console

--- QuitaObstaculosDeCirculo/QuitaObstaculosDeCirculo.ino
+++ QuitaObstaculosDeCirculo/QuitaObstaculosDeCirculo.ino
@@ -1,54 +1,95 @@
 #include <DCMotor.h>
 #include <Ping.h>
 
-//constantes
-#define TRIGGER    500
-#define MAX_MOTOR_SPEED 100.0
-#define MIN_MOTOR_SPEED  0.0
-#define BLACK             
+
+#define MAX_MOTOR_SPEED      100.0
+#define MIN_MOTOR_SPEED      0.0
+#define BLACK_LINE        500 
+#define DINSTANCE_TO_OBJECT     10
+
+#define RIGHT_IR        A0
+#define LEFT_IR         A1
+#define ULTRASOUND        A2
+
+#define TIME_STOP 4000
+#define TIME_BACK 4000
+#define TIME_SPIN 4000
 
 
-//sensors
-#define RIGHT_IR A0
-#define LEFT_IR  A1
-#define ULTRASOUND A2
+int leftSense  = 1;
+int rigthSense = 1;
+int state = 2;
+long currentTime, lastTime = 0;
 
-
-int leftSense = 1,rigthSense = 1;
-
-DCMotor motorLeft(M0_EN, M0_D0, M0_D1);
-DCMotor motorRight(M1_EN, M1_D0, M1_D1);
+DCMotor leftMotor(M0_EN, M0_D0, M0_D1);
+DCMotor rightMotor(M1_EN, M1_D0, M1_D1);
 PingSensor ping(ULTRASOUND);
 
 
 void setup()
 {
   Serial.begin(9600);
-  motorRight.setClockwise(false);
-  motorRight.setSpeed(MIN_MOTOR_SPEED);
-  motorLeft.setSpeed(MIN_MOTOR_SPEED);
+  rightMotor.setClockwise(false);
+  rightMotor.setSpeed(MIN_MOTOR_SPEED);
+  leftMotor.setSpeed(MIN_MOTOR_SPEED);
 }
 
 
 void loop() {
-  //state turn
-  if (){
-      
-  }
+  
+  currentTime = millis(); 
 
-  if (){
+  switch (state)
+  {
+    case 1: //--------STOP--------------------------------------------------------------------
+           if ((currentTime - lastTime ) > TIME_STOP)
+           {
+              state = 3; //BACK
+              rigthSense = -1;
+              leftSense  = -1;
+              lastTime = currentTime;
+              Serial.println("BACK");
+           }
+               break;
 
-  }
+    case 2: //-------FORWARD------------------------------------------------------------------
+           if ((analogRead(RIGHT_IR) > BLACK_LINE) || (analogRead(LEFT_IR) > BLACK_LINE)) 
+           {
+              state = 1; //STOP
+              rigthSense = 0;
+              leftSense  = 0;
+              lastTime = currentTime;
+              Serial.println("STOP");
+           }
+               break;
+    case 3: //-------BACK---------------------------------------------------------------------
+          if ((currentTime - lastTime ) > TIME_BACK)
+          {
+              state = 4; //SPIN 
+              rigthSense =  1;
+              leftSense  = -1;
+              lastTime = currentTime;
+              Serial.println("SPIN");
+            }
+                break;
 
-  if (){
+    case 4: //-------SPIN----------------------------------------------------------------------
+          if (((currentTime - lastTime ) > TIME_SPIN) || (ping.measureCM() < DINSTANCE_TO_OBJECT))
+          {
+              state = 2; //FORWARD
+              rigthSense = 1;
+              leftSense  = 1;
+              lastTime = currentTime;
+              Serial.println("FORWARD");
+            }
+                break;
+    }
 
-  }
+    
+    rightMotor.setSpeed(MAX_MOTOR_SPEED*(-rigthSense));
+    leftMotor.setSpeed(MAX_MOTOR_SPEED*leftSense);
 
-  if (){
-
-  }
-
-  motorRight.setSpeed(MAN_MOTOR_SPEED*rigthSense);
-    motorLeft.setSpeed(MAN_MOTOR_SPEED*leftSense);
 
 }
+
+
